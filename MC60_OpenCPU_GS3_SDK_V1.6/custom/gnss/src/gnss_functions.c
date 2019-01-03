1852787c2d36b87e435ee83b8b8d1d8d11948f91
unknown <contrbas@gmail.com>
2019-01-03 14:19:41 +0200

    gnss final before norms

--- MC60_OpenCPU_GS3_SDK_V1.6/custom/gnss/src/gnss_functions.c
+++ MC60_OpenCPU_GS3_SDK_V1.6/custom/gnss/src/gnss_functions.c
@@ -6,14 +6,14 @@ void	activate_gnss(void)
 
 	ret = RIL_GPS_Open(1);
 
-	if (ret == RIL_AT_SUCCESS)
-	{
-		APP_DEBUG("%s\r\n","gnss activated successfully\0");
-	}
-	else
-	{
-		APP_DEBUG("%s\r\n","gnss activation failure\0");
-	}
+	// if (ret == RIL_AT_SUCCESS)
+	// {
+	// 	APP_DEBUG("%s\r\n","gnss activated successfully\0");
+	// }
+	// else
+	// {
+	// 	APP_DEBUG("%s\r\n","gnss activation failure\0");
+	// }
 }
 
 void	deactivate_gnss(void)
@@ -22,14 +22,14 @@ void	deactivate_gnss(void)
 
 	ret = RIL_GPS_Open(0);
 
-	if (ret == RIL_AT_SUCCESS)
-	{
-		APP_DEBUG("%s\r\n","gnss deactivated successfully\0");
-	}
-	else
-	{
-		APP_DEBUG("%s\r\n","gnss deactivation failure\0");
-	}
+	// if (ret == RIL_AT_SUCCESS)
+	// {
+	// 	APP_DEBUG("%s\r\n","gnss deactivated successfully\0");
+	// }
+	// else
+	// {
+	// 	APP_DEBUG("%s\r\n","gnss deactivation failure\0");
+	// }
 }
 
 bool	gnss_read(void)
@@ -38,30 +38,18 @@ bool	gnss_read(void)
 	u8	rdBuff[1000] = {"\0"};
     u8	item[10] = {"\0"};
 
-    // Ql_memset(rdBuff,0,sizeof(rdBuff));
-    // Ql_strncpy(item,m_RxBuf_Uart+9,3);
-
-    // iRet = RIL_GPS_Read(item,rdBuff);
-    // if(RIL_AT_SUCCESS != iRet)
-
-    // Ql_memset(rdBuff, '\0', sizeof(rdBuff));
-    // Ql_memset(item, '\0', sizeof(item));
-
-    // Ql_sprintf(item, "%s", "RMC");
     Ql_strncpy(item, "RMC", 3);
-
-    APP_DEBUG("item -> %s\r\n", item);
 
 	ret = RIL_GPS_Read(item, rdBuff);
 
 	if (ret == RIL_AT_SUCCESS)
 	{
-		APP_DEBUG("GNSS data -> %s\r\n", rdBuff);
+		// APP_DEBUG("GNSS data -> %s\r\n", rdBuff);
 		return (TRUE);
 	}
 	else
 	{
-		APP_DEBUG("%s\r\n", "gnss data failure\0");
+		// APP_DEBUG("%s\r\n", "gnss data failure\0");
 		return (FALSE);
 	}
 }
--- MC60_OpenCPU_GS3_SDK_V1.6/custom/gnss/src/timer.c
+++ MC60_OpenCPU_GS3_SDK_V1.6/custom/gnss/src/timer.c
@@ -4,7 +4,7 @@ static  bool gnss_activation;
 
 void    timer_GNSS_check_data(u32 timerId, void *param)
 {
-    APP_DEBUG("%s\r\n", "data timer\0");
+    // APP_DEBUG("%s\r\n", "data timer\0");
 
     bool    check_result = FALSE;
 
@@ -12,17 +12,17 @@ void    timer_GNSS_check_data(u32 timerId, void *param)
 
     if (check_result)
     {
-        Ql_OS_SendMessage(subtask1_id, GNSS_TIMER_STOP, 0, 0);
-
         gnss_activation = FALSE;
 
-        Ql_Timer_Stop(GNSS_check_data);   
+        Ql_OS_SendMessage(subtask1_id, GNSS_TIMER_STOP, 0, 0);
+
+        Ql_Timer_Stop(GNSS_check_data); 
     }
 }
 
 void    timer_GNSS_check_general(u32 timerId, void *param)
 {
-    APP_DEBUG("%s\r\n", "general timer\0");
+    APP_DEBUG("%s\r\n", "general timer started\0");
 
     if (!gnss_activation)
     {
@@ -36,14 +36,14 @@ void    timer_GNSS_check_general(u32 timerId, void *param)
 
         ret = Ql_Timer_Start(GNSS_check_data, GNSS_check_data_repeat, TRUE);
 
-        if (ret < 0)
-        {
-            APP_DEBUG("%s\r\n", "GNSS_check_data start failure\0");
-        }
-        else
-        {
-            APP_DEBUG("%s\r\n", "GNSS_check_data start success\0");
-        }
+        // if (ret < 0)
+        // {
+        //     APP_DEBUG("%s\r\n", "GNSS_check_data start failure\0");
+        // }
+        // else
+        // {
+        //     APP_DEBUG("%s\r\n", "GNSS_check_data start success\0");
+        // }
     }
 }
 
@@ -54,14 +54,16 @@ void    timer_GNSS_register_timers(void)
     s32 ret;
     
     ret = Ql_Timer_Register(GNSS_check_general, timer_GNSS_check_general, NULL);
-    if (ret < 0)
-            APP_DEBUG("%s\r\n", "GNSS_check_general register failure");
-    APP_DEBUG("%s\r\n", "GNSS_check_general register success");
+
+    // if (ret < 0)
+    //         APP_DEBUG("%s\r\n", "GNSS_check_general register failure");
+    // APP_DEBUG("%s\r\n", "GNSS_check_general register success");
     
     ret = Ql_Timer_Register(GNSS_check_data, timer_GNSS_check_data, NULL);
-     if (ret < 0)
-            APP_DEBUG("%s\r\n", "GNSS_check_data register failure");
-    APP_DEBUG("%s\r\n", "GNSS_check_data register success");
+    
+    //  if (ret < 0)
+    //         APP_DEBUG("%s\r\n", "GNSS_check_data register failure");
+    // APP_DEBUG("%s\r\n", "GNSS_check_data register success");
 
     Ql_Timer_Start(GNSS_check_general, GNSS_check_general_repeat, TRUE);
 }
