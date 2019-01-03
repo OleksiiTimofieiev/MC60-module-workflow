656893f87fb9aa5edd861e3c01bc17847f974adb
unknown <contrbas@gmail.com>
2019-01-03 19:51:18 +0200

    gnss tol

--- MC60_OpenCPU_GS3_SDK_V1.6/custom/gnss/inc/project.h
+++ MC60_OpenCPU_GS3_SDK_V1.6/custom/gnss/inc/project.h
@@ -1,8 +1,8 @@
 #ifndef __PROJECT_H__
 #define __PROJECT_H__
 
-#define INITIAL_GNSS	0x1008
-#define GNSS_check_data     0x202
+#define INITIAL_GNSS				0x1008
+#define GNSS_check_data   			0x202
 #define GNSS_check_data_repeat      1000
 
 void	deactivate_gnss(void);
